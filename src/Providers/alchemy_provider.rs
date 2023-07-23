a7c9ba90747b7d306842e8b56ea1c50778467dd0
Gus Gray <gusthegray@gmail.com>
2023-07-23 12:19:48 -0700

    multi-threaded (I hope) the loading.

--- src/Providers/alchemy_provider.rs
+++ src/Providers/alchemy_provider.rs
@@ -2,12 +2,11 @@
 // The provider consists of a singleton connection which is used to retrieve data from the API
 
 use ::std::error::Error;
-use ethers::{
-    providers::{Middleware, Provider, Ws},
-    types::Transaction,
-};
+use ethers::providers::{Middleware, Provider, Ws};
 use std::sync::Arc;
 use tokio::sync::Mutex;
+
+use crate::entities::TransactionEntity;
 
 pub struct AlchemyProvider {
     pub provider: Arc<Mutex<Provider<Ws>>>,
@@ -22,21 +21,56 @@ impl AlchemyProvider {
     }
 
     //Get all transactions associated with a given block number
-    pub async fn get_block_transactions(&self, block_number: u64) -> Vec<Transaction> {
+    pub async fn get_block_transactions(
+        &self,
+        block_number: u64,
+    ) -> Result<Vec<TransactionEntity>, Box<dyn Error + Send + Sync>> {
         let provider = self.provider.lock().await;
         let block = provider
             .get_block_with_txs(block_number)
-            .await
-            .unwrap()
-            .unwrap();
-        let transactions = block.transactions;
-        transactions
+            .await?
+            .ok_or("Block not found")?;
+        Ok(block
+            .transactions
+            .into_iter()
+            .map(|tx| TransactionEntity::from(tx))
+            .collect())
     }
 
     //get the latest block number
-    pub async fn get_latest_block_number(&self) -> u64 {
+    pub async fn get_latest_block_number(&self) -> Result<u64, Box<dyn Error + Send + Sync>> {
         let provider = self.provider.lock().await;
-        let block_number = provider.get_block_number().await.unwrap();
-        block_number.as_u64()
+        let block_number = provider.get_block_number().await?;
+        Ok(block_number.as_u64())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::settings;
+
+    #[tokio::test]
+    async fn test_get_block_transactions() {
+        let settings = settings::Settings::new().expect("Unable to collect configs:");
+        let alchemy_provider = AlchemyProvider::new(settings.alchemy_websocket.url)
+            .await
+            .expect("Unable to init provider");
+        let block_number = 17500980;
+        let txns = alchemy_provider
+            .get_block_transactions(block_number)
+            .await
+            .expect("Unable to get block transactions");
+        assert!(!txns.is_empty());
+    }
+
+    #[tokio::test]
+    async fn test_get_latest_block_number() {
+        let settings = settings::Settings::new().expect("Unable to collect configs:");
+        let alchemy_provider = AlchemyProvider::new(settings.alchemy_websocket.url)
+            .await
+            .expect("Unable to init provider");
+        let block_number = alchemy_provider.get_latest_block_number().await;
+        assert!(block_number.is_ok());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,66 @@
+use crossbeam::channel::bounded;
 use eth_mapper::entities::TransactionEntity;
 use eth_mapper::providers::alchemy_provider::AlchemyProvider;
 use eth_mapper::providers::neo4j_repo::GraphRepository;
 use eth_mapper::settings;
 use futures::future::join_all;
-use futures::stream::{self, StreamExt};
 use indicatif::{ProgressBar, ProgressStyle};
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
     //establish a new instance of AlchemyProvider
     let settings = settings::Settings::new().expect("Unable to collect configs:");
     let alchemy_provider = AlchemyProvider::new(settings.alchemy_websocket.url)
-    .await
-    .expect("Unable to init provider");
+        .await
+        .expect("Unable to init provider");
 
-    let starting_block = 17500980;
     let number_of_blocks = 100;
 
-    println!(
-        "=== getting {} blocks starting at {} ===",
-        number_of_blocks, starting_block
-    );
-
     //Get latest block number and set to starting_block
-    let starting_block = alchemy_provider.get_latest_block_number().await;
+    let latest_block = alchemy_provider
+        .get_latest_block_number()
+        .await
+        .expect("Unable to get latest block number");
+    let starting_block = latest_block - number_of_blocks;
 
-    let futures = (starting_block..starting_block - number_of_blocks)
+    print!("=== getting {} blocks, starting with latest block {} and going back ===", number_of_blocks, starting_block);
+
+    let futures = (starting_block..latest_block)
         .map(|block_number| alchemy_provider.get_block_transactions(block_number));
-    let all_txns = join_all(futures)
+
+    let all_txns: Vec<TransactionEntity> = join_all(futures)
         .await
         .into_iter()
         .flatten()
-        .collect::<Vec<_>>();
+        .flatten()
+        .collect();
 
     println!("=== got {} transactions ===", all_txns.len());
 
-    println!("=== connecting to neo4j ===");
-    //establish a new instance of GraphRepository
-    let graph = GraphRepository::new(
-        settings.db_settings.url.as_str(),
-        settings.db_settings.user.as_str(),
-        settings.db_settings.password.as_str(),
-    )
-    .await
-    .unwrap();
-
     println!("=== loading transactions into neo4j ===");
 
-    //load the transactions into the graph
+    //prepare to load the transactions into the graph
+
+    let batch_size = 100;
+
+    let graph = Arc::new(tokio::sync::Mutex::new(
+        GraphRepository::new(
+            settings.db_settings.url.as_str(),
+            settings.db_settings.user.as_str(),
+            settings.db_settings.password.as_str(),
+        )
+        .await
+        .unwrap(),
+    ));
+
+    //load up the transactions using several parallel streams
+
+    // Send transactions to the thread using the channel
+
+    let (sender, receiver) = bounded::<TransactionEntity>(batch_size);
+
+    //setup progress bar
     let pb = ProgressBar::new(all_txns.len() as u64);
     pb.set_style(
         ProgressStyle::default_bar()
@@ -56,24 +69,35 @@ async fn main() {
             .progress_chars("#>-"),
     );
 
-    let batch_size = 100;
-
-    let txn_stream = stream::iter(all_txns.into_iter());
-
-    txn_stream
-        .chunks(batch_size)
-        .for_each_concurrent(None, |txn_batch| async {
-            let txn_entities: Vec<_> = txn_batch
-                .into_iter()
-                .map(|txn| TransactionEntity::from(txn))
-                .collect();
-            graph
-                .load_transactions(&txn_entities)
-                .await
-                .unwrap_or_else(|e| {
-                    println!("error loading transactions: {:?}", e);
-                });
-            pb.inc(txn_entities.len() as u64);
+    // Spawn several threads to process the transactions
+    let workers = (0..5)
+        .map(|_| {
+            let receiver = receiver.clone();
+            let graph = graph.clone();
+            tokio::spawn(async move {
+                while let Ok(txn) = receiver.recv() {
+                    let graph = graph.lock().await;
+                    graph
+                        .load_transaction(&txn)
+                        .await
+                        .expect(format!("Unable to load transaction: {:?}", txn).as_str());
+                }
+            })
         })
-        .await;
+        .collect::<Vec<_>>();
+
+    // Send the transactions to the threads
+    for txn in all_txns {
+        sender.send(txn).unwrap();
+    }
+
+    // Drop the sender so the threads can finish
+    drop(sender);
+
+    // Wait for the threads to finish
+    for worker in workers {
+        worker.await.unwrap();
+    }
+
+    println!("=== done ===");
 }
